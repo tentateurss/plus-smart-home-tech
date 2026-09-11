5431bbefc43615a805ad00d18a5a7059010db6d7
tentateursss <Mativ4ik77@yandex.ru>
2026-09-11 10:30:29 +0300

    Sprint 19: реализован сервис

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/model/hub/ScenarioAddedEvent.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.collector.model.hub;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class ScenarioAddedEvent extends HubEvent {
+
+    @NotBlank
+    private String name;
+
+    @NotEmpty
+    private List<ScenarioCondition> conditions;
+
+    @NotEmpty
+    private List<DeviceAction> actions;
+
+    @Override
+    public HubEventType getType() {
+        return HubEventType.SCENARIO_ADDED;
+    }
+}
---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/service/EventService.java
@@ -0,0 +1,40 @@
+package ru.yandex.practicum.collector.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.collector.config.KafkaTopics;
+import ru.yandex.practicum.collector.model.hub.HubEvent;
+import ru.yandex.practicum.collector.model.sensor.SensorEvent;
+import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
+import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EventService {
+
+    private final SensorEventConverter sensorEventConverter;
+    private final HubEventConverter hubEventConverter;
+    private final KafkaEventProducer kafkaEventProducer;
+
+    public void processSensorEvent(SensorEvent event) {
+        log.debug("Обработка события датчика: {}", event);
+
+        SensorEventAvro avro = sensorEventConverter.convertToAvro(event);
+
+        kafkaEventProducer.send(KafkaTopics.SENSORS_TOPIC, event.getHubId(), avro);
+
+        log.info("Событие датчика {} успешно обработано и отправлено в Kafka", event.getId());
+    }
+
+    public void processHubEvent(HubEvent event) {
+        log.debug("Обработка события хаба: {}", event);
+
+        HubEventAvro avro = hubEventConverter.convertToAvro(event);
+
+        kafkaEventProducer.send(KafkaTopics.HUBS_TOPIC, event.getHubId(), avro);
+
+        log.info("Событие хаба {} успешно обработано и отправлено в Kafka", event.getHubId());
+    }
+}
