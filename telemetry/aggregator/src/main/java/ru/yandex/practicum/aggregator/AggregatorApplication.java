afecdac418f2dcba19f7215606ae311c63678ab9
tentateursss <Mativ4ik77@yandex.ru>
2026-09-23 13:51:31 +0300

    Sprint 21: реализован сервис Aggregator

---
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/AggregatorApplication.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.yandex.practicum.aggregator.service.AggregationStarter;
+
+@SpringBootApplication
+public class AggregatorApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context =
+                SpringApplication.run(AggregatorApplication.class, args);
+
+        AggregationStarter aggregator = context.getBean(AggregationStarter.class);
+        aggregator.start();
+    }
+}
---
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/deserializer/BaseAvroDeserializer.java
@@ -0,0 +1,41 @@
+package ru.yandex.practicum.aggregator.deserializer;
+
+import org.apache.avro.Schema;
+import org.apache.avro.io.BinaryDecoder;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Deserializer;
+
+public class BaseAvroDeserializer<T extends SpecificRecordBase> implements Deserializer<T> {
+
+    private final DecoderFactory decoderFactory;
+    private final Schema schema;
+
+    public BaseAvroDeserializer(Schema schema) {
+        this(DecoderFactory.get(), schema);
+    }
+
+    public BaseAvroDeserializer(DecoderFactory decoderFactory, Schema schema) {
+        this.decoderFactory = decoderFactory;
+        this.schema = schema;
+    }
+
+    @Override
+    public T deserialize(String topic, byte[] data) {
+        if (data == null) {
+            return null;
+        }
+
+        try {
+            BinaryDecoder decoder = decoderFactory.binaryDecoder(data, null);
+            DatumReader<T> reader = new SpecificDatumReader<>(schema);
+            return reader.read(null, decoder);
+        } catch (Exception e) {
+            throw new SerializationException(
+                    "Ошибка десериализации данных для топика [" + topic + "]", e);
+        }
+    }
+}
